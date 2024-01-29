ad7b891e12d44a113fad894efb47b5da78d8e367
jose enrique <67805028+josegarcia2030@users.noreply.github.com>
2024-01-29 15:25:54 -0400

    Atributo style en js

---
+++ 08_DOM/estilos/estilos.js
@@ -0,0 +1,10 @@
+
+const titulo = document.querySelector(".titulo");
+titulo.style.color = "tomato";
+titulo.style.backgroundColor = "#222";
+titulo.style.padding = "20px";
+
+const parrafo = document.querySelector(".parrafo");
+parrafo.style.color = "#222";
+parrafo.style.padding = "30px";
+parrafo.style.backgroundColor = "#ccc";
